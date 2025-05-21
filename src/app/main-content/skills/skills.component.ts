f01ef758e1c371f54d79134bf5552b5e99f54a11
Matthias Hofer <matt.refoh@gmail.com>
2025-05-21 19:17:27 +0200

    finished my skills section with example text

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  skillIcons: string[] = [
+    'angular-skill.svg',
+    'typescript-skill.svg',
+    'javascript-skill.svg',
+    'html-skill.svg',
+    'css-skill.svg',
+    'restapi-skill.svg',
+    'firebase-skill.svg',
+    'git-skill.svg',
+    'material-skill.svg',
+    'scrum-skill.svg',
+  ];
 }

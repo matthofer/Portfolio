dd9e590217a87a67254d7f7c6991f3b34a14f1b8
Matthias Hofer <matt.refoh@gmail.com>
2025-05-22 16:21:42 +0200

    added footer and added functionality to links

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,6 +5,7 @@ import { WhyMeComponent } from './why-me/why-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
@@ -16,6 +17,7 @@ import { ContactComponent } from './contact/contact.component';
     SkillsComponent,
     ProjectsComponent,
     ContactComponent,
+    FooterComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',

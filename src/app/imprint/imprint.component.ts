82fed51eb98d8fd56534f72580847fdd8e0c35cc
Matthias Hofer <matt.refoh@gmail.com>
2025-05-22 16:52:45 +0200

    added imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
-
+  email = 'contact@matthias-hofer.com';
 }

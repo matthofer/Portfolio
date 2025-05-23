ff1d0e85f429ffb2af03086d853f909565efe034
Matthias Hofer <matt.refoh@gmail.com>
2025-05-23 19:24:09 +0200

    made imprint responsive and added translations

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,15 +1,79 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { HeaderComponent } from '../shared/header/header.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, TranslateModule],
+  imports: [HeaderComponent, FooterComponent, TranslateModule, CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
+  constructor(private translate: TranslateService) {}
   email = 'contact@matthias-hofer.com';
+  activeLang: string = 'en';
+  menuOpen = false;
+  currentIcon = 'assets/img/icons/burger-default.svg';
+
+  setLanguage(lang: string) {
+    this.activeLang = lang;
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
+
+  ngOnInit() {
+    const savedLang = localStorage.getItem('lang') || 'en';
+    this.translate.use(savedLang);
+    this.activeLang = savedLang;
+    window.addEventListener('resize', this.handleResize.bind(this));
+    this.handleResize();
+  }
+
+  handleResize(): void {
+    if (window.innerWidth >= 1080 && this.menuOpen) {
+      this.menuOpen = false;
+    }
+  }
+
+  toggleMenu(): void {
+    if (!this.menuOpen) {
+      this.animateToClose();
+      this.menuOpen = true;
+    } else {
+      this.animateToBurger();
+      this.menuOpen = false;
+    }
+  }
+
+  animateToClose(): void {
+    const frames = [
+      'assets/img/icons/burger-opening.svg',
+      'assets/img/icons/burger-closing.svg',
+      'assets/img/icons/burger-close.svg',
+    ];
+    this.playIconFrames(frames);
+  }
+
+  animateToBurger(): void {
+    const frames = [
+      'assets/img/icons/burger-closing.svg',
+      'assets/img/icons/burger-opening.svg',
+      'assets/img/icons/burger-default.svg',
+    ];
+    this.playIconFrames(frames);
+  }
+
+  playIconFrames(frames: string[]): void {
+    let i = 0;
+    const interval = setInterval(() => {
+      this.currentIcon = frames[i];
+      i++;
+      if (i === frames.length) {
+        clearInterval(interval);
+      }
+    }, 70);
+  }
 }
